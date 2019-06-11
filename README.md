7ced89a9169400bd9055556ab5a73c95f24a3d9b
Cássio Mazzochi Molin <cassio.molin@gmail.com>
2019-06-12 00:02:55 +0100

    Log configuration

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Log aggregation is a common requirement of a microservices architecture.
 
 Unlike in a monolithic application, a single business operation is split across a number of services. When something goes wrong, it's important to be able to trace each operation across the services.
+
+---
+
+```bash
+docker-compose up
+```
--- gateway-service/src/main/java/com/cassiomolin/logaggregation/gateway/service/GreetingService.java
+++ gateway-service/src/main/java/com/cassiomolin/logaggregation/gateway/service/GreetingService.java
@@ -2,10 +2,12 @@ package com.cassiomolin.logaggregation.gateway.service;
 
 import com.cassiomolin.logaggregation.gateway.domain.Greeting;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GreetingService {
@@ -14,8 +16,12 @@ public class GreetingService {
 
     public Greeting getGreeting() {
 
+        log.info("Requesting greeting");
+
         ResponseEntity<Greeting> responseEntity =
                 restTemplate.getForEntity("http://localhost:8002/greetings", Greeting.class);
+
+        log.info("Found greeting");
 
         return responseEntity.getBody();
     }
